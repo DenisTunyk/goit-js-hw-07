4f23fda97edfa4ee21226532a555ffdb019b687f
DenisTunyk <tundenis43@gmail.com>
2022-10-31 12:04:23 +0200

    example_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onClickGallery);
+
+const markup = galleryItems.map(({original, preview, description}) =>
+    `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+function onClickGallery(e) {
+    e.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery a',{captionsData: "alt", captionDelay: 250,});
+        gallery.on('show.simplelightbox', function () {
+	    // do something…
+    });
+}
+
+
+
