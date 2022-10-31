414188828076a4e0a85c331dad26490328c25447
DenisTunyk <tundenis43@gmail.com>
2022-10-31 11:35:43 +0200

    homework_example1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onClickGallery);
+
+const markup = galleryItems.map(({original, preview, description}) =>
+    `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+        </a>
+    </div>
+    `).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+function onClickGallery(e) {
+    e.preventDefault();
+
+    basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`).show();
+}
+
+
+
+
